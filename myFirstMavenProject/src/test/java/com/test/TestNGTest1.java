b5ae1a49c1483fdcfdd28cffc0e698ff3d51f955
Sudhir123 <Sudhir123@Sudhir>
2022-09-27 23:53:10 +0530

    adding a DEMO PROJECT

---
+++ myFirstMavenProject/src/test/java/com/test/TestNGTest1.java
@@ -0,0 +1,22 @@
+package com.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestNGTest1 {
+	
+	@Test
+	public void logintest() {
+		
+//		System.setProperty("webdriver.chrome.driver","C:\\Users\\Sudhir123\\Desktop\\chromedriver_win32 (1)\\chromedriver.exe");
+//		WebDriver driver = new ChromeDriver();
+//		driver.get("file:///C:/Users/Sudhir123/Desktop/Offline%20Website/index.html");
+//		Assert.assertEquals(true, false);
+		
+		System.out.println("hello world");
+		
+	}
+
+}
