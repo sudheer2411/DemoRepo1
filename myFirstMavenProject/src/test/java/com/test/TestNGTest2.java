1ead7c22ddca659787a91315005513fd6511f64d
Sudhir123 <Sudhir123@Sudhir>
2022-09-29 00:49:08 +0530

    adding a new test class

---
+++ myFirstMavenProject/src/test/java/com/test/TestNGTest2.java
@@ -0,0 +1,36 @@
+package com.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestNGTest2 {
+	
+	@Test
+	public void logintest() {
+		
+//		System.setProperty("webdriver.chrome.driver","C:\\Users\\Sudhir123\\Desktop\\chromedriver_win32 (1)\\chromedriver.exe");
+//		WebDriver driver = new ChromeDriver();
+//		driver.get("file:///C:/Users/Sudhir123/Desktop/Offline%20Website/index.html");
+//		Assert.assertEquals(true, false);
+		
+		System.out.println("hello world");
+		
+	}
+	
+	@Test
+	public void logintest2() {
+		
+//		System.setProperty("webdriver.chrome.driver","C:\\Users\\Sudhir123\\Desktop\\chromedriver_win32 (1)\\chromedriver.exe");
+//		WebDriver driver = new ChromeDriver();
+//		driver.get("file:///C:/Users/Sudhir123/Desktop/Offline%20Website/index.html");
+//		Assert.assertEquals(true, false);
+		
+		System.out.println("Hello Cosmos");
+		
+		
+		
+	}
+
+}
