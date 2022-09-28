9a57d39b3b4bd4048ab26ff79270d174edf7a544
Sudhir123 <Sudhir123@Sudhir>
2022-09-29 00:35:36 +0530

    adding new test case

--- myFirstMavenProject/src/test/java/com/test/TestNGTest1.java
+++ myFirstMavenProject/src/test/java/com/test/TestNGTest1.java
@@ -18,5 +18,19 @@ public class TestNGTest1 {
 		System.out.println("hello world");
 		
 	}
+	
+	@Test
+	public void logintest2() {
+		
+//		System.setProperty("webdriver.chrome.driver","C:\\Users\\Sudhir123\\Desktop\\chromedriver_win32 (1)\\chromedriver.exe");
+//		WebDriver driver = new ChromeDriver();
+//		driver.get("file:///C:/Users/Sudhir123/Desktop/Offline%20Website/index.html");
+//		Assert.assertEquals(true, false);
+		
+		System.out.println("Hello Cosmos");
+		
+		
+		
+	}
 
 }
